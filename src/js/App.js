1a11e17cf92e2e1426812a02ef9c9024629d8551
David <David@MacBook-Pro.local>
2015-07-18 18:51:35 -0400

    UI Frontpage

--- src/js/App.js
+++ src/js/App.js
@@ -6,17 +6,20 @@ const info = [
   {
     name: 'Google',
     desc: 'just a test',
-    image: 'https://www.google.ca/images/google_favicon_128.png'
+    image: 'http://dummyimage.com/110',
+    price: '$100,000 For 10%'
   },
   {
     name: 'Apple',
     desc: 'hey',
-    image: 'http://www.milident.com/wp-content/uploads/2012/10/icon_apple.jpg'
+    image: 'http://dummyimage.com/110',
+    price: '$10 For 50%'
   },
   {
     name: 'Facebook',
     desc: 'A cool company',
-    image: 'http://analyticphilosophy.eu/wp-content/plugins/social-media-feather/synved-social/image/social/regular/96x96/facebook.png?b694a8'
+    image: 'http://dummyimage.com/110',
+    price: '$50 For 2%'
   }
 ];
 
@@ -27,7 +30,7 @@ export default class App extends React.Component {
     const items = info.map((object, i) => {
       const string = 'project/' + object.name;
 
-      return <Listing key={i} name={object.name} href={string} desc={object.desc} image = {object.image}/>
+      return <Listing key={i} name={object.name} href={string} desc={object.desc} image = {object.image} price = {object.price}/>
     })
 
     return (
@@ -47,9 +50,16 @@ class Listing extends React.Component {
       <div className="listing">
         <img src={this.props.image}/>
 
-        <div className="content">
-          <a href={this.props.href}>{this.props.name}</a> <p>{this.props.desc}</p>
+        <div className="title">
+          <h2><a href={this.props.href}>{this.props.name}</a></h2>
         </div>
+        <div className="price">
+         <h2>{this.props.price} </h2>
+        </div>
+        <div className = "description">
+          {this.props.desc}
+        </div>
+
       </div>
     )
   }
